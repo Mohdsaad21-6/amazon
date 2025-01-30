4845ed30a7111a7a95c22c76af3420694b825cb7
Mohd Saad <shaadansari8081@gmail.com>
2025-01-30 19:10:04 +0530

    Add Axios for API requests; refactor Cart and Redux actions to use Axios for data fetching

--- client/src/components/cart/Cart.jsx
+++ client/src/components/cart/Cart.jsx
@@ -2,30 +2,31 @@ import { Divider } from "@mui/material";
 import "./cart.css";
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
+import axios from "axios";
 
 const Cart = () => {
-
-
-  const {id} = useParams();
+  const { id } = useParams("");
   // console.log(id);
 
- const  getinddata=async()=>{
-  const  res=await fetch(`/getproductsone/${id}`,{withcredentials:true},
-    {
-      method:"GET",
-      headers:{
-        "Content-Type":"application/json",
+  const getinddata = async () => {
+    try {
+      const res = await axios.get(
+        `http://localhost:8005/getproductsone/${id}`,
+        {
+          withCredentials: true,
+        }
+      );
+
+      const data = await res.data;
+      console.log(data);
+    } catch (error) {
+      console.log(error.message);
     }
-  }
-  );
-  const data=await res.json();
-  console.log(data)
- }
+  };
 
-
-useEffect(()=>{
-  getinddata()
-},[id])
+  useEffect(() => {
+    getinddata();
+  }, [id]);
 
   return (
     <div className="cart_section">
@@ -38,22 +39,55 @@ useEffect(()=>{
           </div>
         </div>
         <div className="right_cart">
+          <h3>Fitness Gear</h3>
+          <h4>ac with best services</h4>
+          <Divider />
+          <p className="mrp">M.R.P ₹ 1195</p>
+          <p>
+            Deal Of The Day:<span style={{ color: "#b12704" }}>₹625.00</span>{" "}
+          </p>
+          <p>
+            You save:<span style={{ color: "#b12704" }}>547 (47%)</span>{" "}
+          </p>
 
-        <h3>Fitness Gear</h3>
-        <h4>ac with best services</h4>
-        <Divider/>
-        <p className="mrp">M.R.P ₹ 1195</p>
-        <p>Deal Of The Day:<span style={{color:"#b12704"}}>₹625.00</span> </p>
-        <p>You save:<span style={{color:"#b12704"}}>547 (47%)</span> </p>
-
-        <div className="discount_box">
-            <h5>Discount : <span style={{color:"#111"}}>Extra 10% 0ff</span></h5>
-            <h4 > Free Deleivery : <span style={{color:"#111" ,fontWeight:"600"}}> Oct 8-21 </span>Details </h4>
-            <p>Fastest Deleivery:<span style={{color:"#111" ,fontWeight:"600"}}> Tommorrow 11 AM</span></p>
-        </div>
-        <p className="description">About The Item <span style={{color:"565959", fontSize:"14px", fontWeight:"500",letterSpacing:"0."}}>The sun was setting over the vast, rolling hills as a gentle breeze rustled the leaves of the tall trees. A group of children laughed and played in the distance, their joyful shouts carrying on the wind. The air was filled with the sweet scent of blooming wildflowers everywhere outside</span></p>
-
-
+          <div className="discount_box">
+            <h5>
+              Discount : <span style={{ color: "#111" }}>Extra 10% 0ff</span>
+            </h5>
+            <h4>
+              {" "}
+              Free Deleivery :{" "}
+              <span style={{ color: "#111", fontWeight: "600" }}>
+                {" "}
+                Oct 8-21{" "}
+              </span>
+              Details{" "}
+            </h4>
+            <p>
+              Fastest Deleivery:
+              <span style={{ color: "#111", fontWeight: "600" }}>
+                {" "}
+                Tommorrow 11 AM
+              </span>
+            </p>
+          </div>
+          <p className="description">
+            About The Item{" "}
+            <span
+              style={{
+                color: "565959",
+                fontSize: "14px",
+                fontWeight: "500",
+                letterSpacing: "0.",
+              }}
+            >
+              The sun was setting over the vast, rolling hills as a gentle
+              breeze rustled the leaves of the tall trees. A group of children
+              laughed and played in the distance, their joyful shouts carrying
+              on the wind. The air was filled with the sweet scent of blooming
+              wildflowers everywhere outside
+            </span>
+          </p>
         </div>
       </div>
     </div>
--- client/src/components/redux/actions/action.js
+++ client/src/components/redux/actions/action.js
@@ -1,15 +1,22 @@
+import axios from "axios";
 export const getProducts = () => async (dispatch) => {
   try {
-    const data = await fetch("http://localhost:8005/getproducts",
-      { withCredentials: true } ,
-      {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-    const res = await data.json();
-    console.log(res);
+    // const data = await fetch("http://localhost:8005/getproducts",
+    //   { withCredentials: true } ,
+    //   {
+    //   method: "GET",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    // })
+    // const res = await data.json();
+    // console.log(res);
+
+    const data = await axios.get("http://localhost:8005/getproducts", {
+      withCredentials: true,
+    });
+
+    const res = data.data;
 
     dispatch({
       type: "SUCCESS_GET_PRODUCTS",
@@ -22,5 +29,3 @@ export const getProducts = () => async (dispatch) => {
     });
   }
 };
-
-
